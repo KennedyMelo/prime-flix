a18be8a637fc91aad2c93d80a0dcedebf2325709
KennedyMelo <kennedycmelo@gmail.com>
2023-08-28 23:38:19 -0300

    feat: delete film in favorite page

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -5,14 +5,25 @@ import { Link } from 'react-router-dom';
 function Favoritos(){
   const [filmes, setFilmes] = useState([])
   
-useEffect(() => {
-  const minhaLista = localStorage.getItem('@primeflix')
-  setFilmes(JSON.parse(minhaLista)  || [])
-}, [])
+  useEffect(() => {
+    const minhaLista = localStorage.getItem('@primeflix')
+    setFilmes(JSON.parse(minhaLista)  || [])
+  }, [])
 
+  function excluirFilme(id){
+    let filtrosFilmes = filmes.filter((item) => {
+      return item.id !== id
+    })
+
+    setFilmes(filtrosFilmes);
+    localStorage.setItem('@primeflix', JSON.stringify(filtrosFilmes))
+
+  }
   return(
     <div className='meus-filmes'>
       <h1>Meus filmes</h1>
+      
+      {filmes.length === 0 && <span>Você não possui nenhum filme salvo</span>}
       
       <ul>
         {filmes.map((item) => {
@@ -21,7 +32,7 @@ useEffect(() => {
               <span>{item.title}</span>
               <div>
                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                <button>Excluir</button>
+                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
               </div>
             </li>
           )
